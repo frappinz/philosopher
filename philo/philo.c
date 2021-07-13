bcbb04d083ac425c32db83891b19a826d6d1cc3d
Francesca Minardi <fminardi@e3r6p1.42roma.it>
2021-07-13 18:38:57 +0200

    looppa un solo filosofo

--- philo/philo.c
+++ philo/philo.c
@@ -1,116 +1,148 @@
 #include "philo.h"
 
-// int N = -1;
-
-// void	*routine(void *i)
-// {
-// 	N++;
-// 	printf("\n%d: sono il thread n %d!\n", (int)i, N);
-// 	return(NULL);
-// }
-
-// int main(int ac, char **av)
-// {
-// 	pthread_t thread[5];
-// 	int i = 0;
-// 	int y;
-
-// 	while (i < 5)
-// 	{
-// 		printf("bella ho creato thread %d\n", i);
-// 		y = pthread_create(&thread[i], NULL, routine, (void *)i);
-// 		if (y)
-// 		{
-// 			printf("Errore!!!!!!!! il ritorno é %d\n", y);
-// 			exit (-1);
-// 		}
-// 		pthread_join(thread[i], NULL);
-// 		i++;
-// 	}
-// 	return (0);
-// }
-
-// #define NUMTHRDS 10
-// pthread_t callThd[NUMTHRDS];
-// pthread_mutex_t mutexdata; int data;
-
-// void *decrementa(void *arg)
-// {
-// 	pthread_mutex_lock (&mutexdata);
-// 	if(data>0)
-// 		data--;
-// 	pthread_mutex_unlock (&mutexdata);
-// 	pthread_exit((void*) 0);
-// }
-
-// int main (int argc, char *argv[])
-// {
-// 	int i; int status; pthread_attr_t attr;
-// 	data=4;
-// 	pthread_mutex_init (&mutexdata, NULL);
-// 	for(i=0;i < NUMTHRDS;i++) 
-// 	{
-// 		// per default i thread consentono il join,
-// 		pthread_create ( &callThd[i], NULL, decrementa, (void *)i);
-// 	}
-// 	for(i=0;i < NUMTHRDS;i++) 
-// 	{ // aspetto la fine dei thread	
-// 		pthread_join ( callThd[i], (void **)&status);
-// 	}
-// 	printf ("data = %d \n", data);
-// 	pthread_mutex_destroy (&mutexdata);
-// 	pthread_exit(NULL);
-// } 
-
-void	*routine(void *var)
+void	*routine(void *table)
 {
-	//(int)i;
-	t_var *cast_var;
-	cast_var = (t_var *)var;
+	t_table *cast_table;
+	cast_table = (t_table *)table;
 
-	pthread_mutex_lock(&cast_var->mutex[cast_var->i]);
-	printf("ho creato %i filosof*\n", cast_var->i);
-	
+	cast_table->philo[cast_table->i].status = 6;
+	//pthread_mutex_init(&cast_table->aspee, NULL);
+	//printf("\n");
+	while (cast_table->philo[cast_table->i].status != 0)
+	{
+		printf("il %i filosof* ", cast_table->i);
+		if(cast_table->philo[cast_table->i].status == 6 || cast_table->philo[cast_table->i].status == THINK) //DEVE MANGIARE
+		{
+			if (cast_table->forchetta[cast_table->i]) //se esiste la forchetta davanti
+			{
+			//printf("la forchetta davanti a lui é libera\n");
+				if (cast_table->i == cast_table->num_phil) //se é l'ultimo filosofo
+				{
+					if (cast_table->forchetta[0])	//se esiste la forchetta alla sua sinistra
+					{
+						//printf("ed é libera anche quella alla sua destra\n");
+						pthread_mutex_lock(&cast_table->fork[0]);	//pigliala!!
+						cast_table->forchetta[0] = 0;
+						pthread_mutex_lock(&cast_table->fork[cast_table->i]);
+						cast_table->forchetta[cast_table->i] = 0;
+						cast_table->philo[cast_table->i].status = EAT;
+						printf("MANGIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
+						usleep(cast_table->te);
+						pthread_mutex_unlock(&cast_table->fork[0]);
+						pthread_mutex_unlock(&cast_table->fork[cast_table->i]);
 
+					}
+					else
+					{
+						printf("dorme\n");
+						cast_table->philo[cast_table->i].status = SLEEP;
+						usleep(cast_table->ts);
+					}
+				}
+				else if (cast_table->forchetta[cast_table->i + 1])
+				{
+					//printf("ed é libera anche quella alla sua destra\n");
+					pthread_mutex_lock(&cast_table->fork[cast_table->i]);
+					pthread_mutex_lock(&cast_table->fork[cast_table->i + 1]);
+					cast_table->forchetta[cast_table->i] = 0;
+					cast_table->forchetta[cast_table->i + 1] = 0;
+					cast_table->philo[cast_table->i].status = EAT;
+					printf("MANGIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
+					usleep(cast_table->te);
+					pthread_mutex_unlock(&cast_table->fork[cast_table->i]);
+					pthread_mutex_unlock(&cast_table->fork[cast_table->i + 1]);
+					cast_table->forchetta[cast_table->i] = 1;
+					cast_table->forchetta[cast_table->i + 1] = 1;
+				}
+				else
+				{
+					printf("dorme\n");
+					cast_table->philo[cast_table->i].status = SLEEP;
+					usleep(cast_table->ts);
+				}
+			}
+			else
+			{
+				if (cast_table->philo[cast_table->i].status == 6)
+				{
+					printf("pensa\n");
+					cast_table->philo[cast_table->i].status = THINK;
+				}
+				else
+				{
+					printf("dorme\n");
+					cast_table->philo[cast_table->i].status = SLEEP;
+					usleep(cast_table->ts);
+				}
+			}
+		}
+		else if (cast_table->philo[cast_table->i].status == EAT)
+		{
+			printf("dorme\n");
+			cast_table->philo[cast_table->i].status = SLEEP;
+			usleep(cast_table->ts);
+		}
+		else if (cast_table->philo[cast_table->i].status == SLEEP)
+		{
+			printf("pensa\n");
+			cast_table->philo[cast_table->i].status = THINK;
+		}
+		else
+		{
+			pthread_mutex_lock(&cast_table->aspee);
+			printf("sono morto\n");
+			cast_table->philo[cast_table->i].status = DIE;
+			exit(0);
+		}
+	}
 	return (NULL);
 }
 
 int main(int argc, char *argv[])
 {
-	t_var var;
-	int i = 1;
-	struct timeval	start;
-	struct timeval	end;
+	t_table *table;
+	int	i;
+	// struct timeval	start;
+	// struct timeval	end;
 	
 	if (argc < 4)
 		return (0);
-	//pthread_mutex_init(&var.mutex, NULL);
-	var.num_phil = ft_atoi(argv[1]);
-	var.td = ft_atoi(argv[2]);
-	var.te = ft_atoi(argv[3]);
-	var.ts = ft_atoi(argv[4]);
-	var.thread = calloc(var.num_phil, sizeof(pthread_t *));
-	var.mutex = calloc(var.num_phil, sizeof(pthread_mutex_t *));
-
+	table = malloc(sizeof(t_table));
+	table->i = 0;
+	table->num_phil = ft_atoi(argv[1]);
+	table->td = ft_atoi(argv[2]);
+	table->te = ft_atoi(argv[3]);
+	table->ts = ft_atoi(argv[4]);
+	table->philo = calloc(table->num_phil, sizeof(t_philo));
+	table->fork = calloc(table->num_phil, sizeof(pthread_mutex_t));
+	table->forchetta = calloc(table->num_phil, sizeof(int));
+	i = 0;
+	while (i < table->num_phil)
+	{
+		table->forchetta[i] = 1;
+		i++;
+	}
+	while (table->i < table->num_phil)
+	{
+		pthread_mutex_init(&table->fork[table->i], NULL);
+		table->i++;
+	}
+	table->i = 0;
+	//pthread_mutex_init(&table->aspee, NULL);
+	while (table->i < table->num_phil)
+	{ 
+		//pthread_mutex_lock(&table->aspee);
+		pthread_create(&table->philo[table->i].thread, NULL, &routine, table);
+		usleep(10);
+		pthread_join(table->philo[table->i].thread, NULL);
+		table->i++;
+		//pthread_mutex_unlock(&table->aspee);
+	}
+	return (0);
+}
+	
 	// gettimeofday(&start, NULL);
 	// sleep(1);
 	// gettimeofday(&end, NULL);
 	// printf("sono passati %li microsecondi\n", ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
 	//printf("h: %ld,%d\nnumber_of_philosophers: %i\ntime_to_die: %d\ntime_to_eat %d\ntime_to_sleep %d\n", current_time.tv_sec, current_time.tv_usec, num_phil, td, te, ts);
-	while (var.i <= var.num_phil)
-	{
-		pthread_mutex_init(&var.mutex[var.i], NULL);
-		//pthread_join(thread[i], NULL); STA COSA FA AVERE A TUTTI LO STESSO ID!!
-		//printf("thread[%i]  = %i\n", i, thread[i]);
-		i++;
-	}
-	i = 0;
-	while (var.i <= var.num_phil)
-	{
-		pthread_create(&var.thread[var.i], NULL, routine, &var);
-		pthread_join(var.thread[var.i], NULL);
-		//printf("thread[%i]  = %i\n", i, thread[i]);
-		var.i++;
-	}
-	return (0);
-}
--- philo/philo.h
+++ philo/philo.h
@@ -6,23 +6,43 @@
 # include <stdio.h>
 # include <pthread.h>
 # include <sys/time.h>
+# include <string.h>
 
-typedef	struct	s_timeval	
-{
-	time_t	tv_sec;
-	suseconds_t	tv_usec;
-}t_timeval;
+// typedef	struct	s_timeval	
+// {
+// 	time_t	tv_sec;
+// 	suseconds_t	tv_usec;
+// }t_timeval;
 
-typedef struct	s_var
+enum e_status
 {
-	int i;
-	int num_phil;
-	suseconds_t td;
-	suseconds_t te;
-	suseconds_t ts;
-	pthread_t *thread;
-	pthread_mutex_t    *mutex;
-} t_var;
+	DIE,
+	SLEEP,
+	EAT,
+	THINK
+};
+
+typedef	struct s_philo
+{
+	pthread_t		thread;
+	enum e_status	status;
+	int				index;
+
+}t_philo;
+
+typedef struct	s_table
+{
+	int 				i;
+	int 				num_phil;
+	suseconds_t 		td;
+	suseconds_t 		te;
+	suseconds_t 		ts;
+	int					*forchetta;
+	t_philo				*philo;
+	pthread_mutex_t    	*fork;
+	pthread_mutex_t    	print;
+	pthread_mutex_t		aspee;
+}t_table;
 
 int	ft_atoi(const char *str);
 int main(int argc, char *argv[]);
